f5232276b2b1a4c75a802ffa032a16a98a9cd26e
timawesomeness <timawesomeness@users.noreply.github.com>
2018-04-06 12:06:48 -0600

    Update bot_novote.py

--- bot_novote.py
+++ bot_novote.py
@@ -8,25 +8,38 @@ pw = getpass.getpass()
 reddit = praw.Reddit(user_agent="XXX", client_id="XXX", client_secret="XXX", username="XXX", password=pw)
 
 count = 0
-
-for i in reddit.subreddit("circleoftrust").stream.submissions():
-    if not i.title.startswith("u/"): # People with username circles probably aren't setting the key to that
-        try:
-            output = reddit.post("/api/guess_voting_key.json", data=dict(id=i.name, raw_json=1, vote_key=i.title))
-            key = i.title
-            if not output.get(key) and key and key != "" and len(key.split()) > 1:
-                first_output = output
-                output = reddit.post("/api/guess_voting_key.json", data=dict(id=i.name, raw_json=1, vote_key=i.title.split()[-1]))
-                key = i.title.split()[-1]
-            else:
-                first_output = None
-        except prawcore.exceptions.NotFound:
-            continue
-        with open("circles.txt", "a") as f:
-            f.write(i.title + ": " + (str(first_output) + ", " if first_output else "") + str(output) + "\n")
-        if output.get(key):
-            print(f"Got one: {key}")
-            webbrowser.open(f"https://reddit.com/{i.id}") # Open a web browser to allow manual voting
-        count += 1
-        if count % 25 == 0:
-            print(count)
+while True:
+    try:
+        for i in reddit.subreddit("circleoftrust").stream.submissions():
+            outputs = []
+            output = []
+            key = ""
+            if not i.title.startswith("u/") and i.link_flair_text != "Betrayed":                                   # " ".join(i.title.split()[:2]), 
+                for j in [i.title]+([i.title.split()[0], i.title.split()[-1]] if len(i.title.split()) > 1 else [])+([" ".join(i.title.split()[-2:])] if len(i.title.split()) > 3 else []):
+                    if not j:
+                        continue
+                    try:
+                        output = reddit.post("/api/guess_voting_key.json", data=dict(id=i.name, raw_json=1, vote_key=j))
+                        outputs.append(output)
+                        if output.get(j):
+                            key = j
+                            break
+                    except prawcore.exceptions.NotFound:
+                        continue
+                with open("circles.txt", "a") as f:
+                    f.write(i.title + ": " + str(outputs) + "\n")
+                if output.get(key):
+                    print(f"Got one: {key}")
+                    try:
+                        with open("joinable_circles.txt", "a") as f:
+                            f.write(f"https://www.reddit.com/user/{i.author}/circle/embed\n")
+                        webbrowser.open(f"https://www.reddit.com/user/{i.author}/circle/embed")
+                    except Exception as e:
+                        print(f"Error: {str(e)}")
+                count += 1
+                if count % 25 == 0:
+                    print(count)
+    except KeyboardInterrupt:
+        break
+    except:
+        pass
